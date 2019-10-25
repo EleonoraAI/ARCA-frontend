b658668824a26b84024fb176618f894188e0b46d
Eleonora Bernasconi <eleonorameetup@gmail.com>
2019-10-25 17:48:53 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,48 +6,48 @@ var Ontodia = require('ontodia')
 //require('jointjs/css/themes/default.css');
 
 function onWorkspaceMounted(workspace) {
-    if (!workspace) {
-        return;
-    }
+  if (!workspace) {
+    return;
+  }
 
-    const model = workspace.getModel();
+  const model = workspace.getModel();
 
-    // model.graph.on('action:iriClick', (iri) => {
-    //     window.open(iri);
-    //     console.log(iri);
-    // });
+  // model.graph.on('action:iriClick', (iri) => {
+  //     window.open(iri);
+  //     console.log(iri);
+  // });
 
-    //c'è un modo di fare la ricerca solo sullo sparql endpoint locale???
-    //vedere l'integrazione del codice!!!
-    //modularizzazione react macrocomponenti 1 ontodia 2 libreria etc.
+  //c'è un modo di fare la ricerca solo sullo sparql endpoint locale???
+  //vedere l'integrazione del codice!!!
+  //modularizzazione react macrocomponenti 1 ontodia 2 libreria etc.
 
-    model.importLayout({
-        validateLinks: true,
-        dataProvider: new Ontodia.CompositeDataProvider([
-            new Ontodia.SparqlDataProvider({
-                endpointUrl: '/sparql-endpoint',
-                queryMethod: Ontodia.SparqlQueryMethod.GET
-            }, Ontodia.OWLStatsSettings),
-            new Ontodia.SparqlDataProvider({
-                endpointUrl: 'http://dbpedia.org/sparql',
-                imagePropertyUris: [
-                    'http://xmlns.com/foaf/0.1/depiction',
-                    'http://xmlns.com/foaf/0.1/img',
-                ],
-                queryMethod: Ontodia.SparqlQueryMethod.GET,
-                fullTextSearch: {
-                    prefix: 'PREFIX dbo: <http://schema.org/LERMAbook/>\n',
-                    queryPattern: `
+  model.importLayout({
+    validateLinks: true,
+    dataProvider: new Ontodia.CompositeDataProvider([
+      new Ontodia.SparqlDataProvider({
+        endpointUrl: '/sparql-endpoint',
+        queryMethod: Ontodia.SparqlQueryMethod.GET
+      }, Ontodia.OWLStatsSettings),
+      new Ontodia.SparqlDataProvider({
+        endpointUrl: 'http://dbpedia.org/sparql',
+        imagePropertyUris: [
+          'http://xmlns.com/foaf/0.1/depiction',
+          'http://xmlns.com/foaf/0.1/img',
+        ],
+        queryMethod: Ontodia.SparqlQueryMethod.GET,
+        fullTextSearch: {
+          prefix: 'PREFIX dbo: <http://schema.org/LERMAbook/>\n',
+          queryPattern: `
                           ?inst rdfs:label ?searchLabel.
                           ?searchLabel bif:contains "\${text}".
                     `,
-                },
-                classTreeQuery: `
+        },
+        classTreeQuery: `
                     SELECT distinct ?LERMAbook ?label WHERE {
                         ?LERMAbook rdfs:label ?label.
                     }
                 `,
-                elementInfoQuery: `
+        elementInfoQuery: `
                     CONSTRUCT {
                         ?inst rdf:type ?LERMAbook .
                         ?inst rdfs:label ?label .
@@ -61,90 +61,90 @@ function onWorkspaceMounted(workspace) {
                         FILTER (isLiteral(?propValue)) }
                     }               
                 `,
-                filterElementInfoPattern: `
+        filterElementInfoPattern: `
                     OPTIONAL {?inst rdf:type ?foundClass. FILTER (!contains(str(?foundClass), 'http://dbpedia.org/class/yago'))}
                     BIND (coalesce(?foundClass, owl:Thing) as ?class)
                     OPTIONAL {?inst \${dataLabelProperty} ?label}`,
-                imageQueryPattern: ` { ?inst ?linkType ?fullImage } UNION { [] ?linkType ?inst. BIND(?inst as ?fullImage) }
+        imageQueryPattern: ` { ?inst ?linkType ?fullImage } UNION { [] ?linkType ?inst. BIND(?inst as ?fullImage) }
                         BIND(CONCAT("https://commons.wikimedia.org/w/thumb.php?f=",
                         STRAFTER(STR(?fullImage), "Special:FilePath/"), "&w=200") AS ?image)
                 `,
-            }, Ontodia.DBPediaSettings),
-        ]),
-    });
+      }, Ontodia.DBPediaSettings),
+    ]),
+  });
 }
 
 const props = {
-    ref: onWorkspaceMounted,
-    languages: [{
-            code: 'it',
-            label: 'Italiano'
-        },
-        {
-            code: 'en',
-            label: 'Inglese'
-        },
-        // {
-        //     code: 'de',
-        //     label: 'Tedesco'
-        // },
-        // {
-        //     code: 'ru',
-        //     label: 'Russo'
-        // },
-    ],
-    language: 'it',
-    viewOptions: {
-        onIriClick: ({
-            iri
-        }) => window.open(iri),
-        // groupBy: [{
-        //     linkType: 'http://www.researchspace.org/ontology/group',
-        //     linkDirection: 'in'
-        // }, ],
+  ref: onWorkspaceMounted,
+  languages: [{
+      code: 'it',
+      label: 'Italiano'
     },
-    typeStyleResolver: types => {
-        //BOOK
-        if (types.indexOf('http://schema.org/LERMAbook') !== -1) {
-            return {
-                color: '#80040a'
-            };
-        //CONCEPTS
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMATopConcept') !== -1) {
-            return {
-                color: '#00FF00'
-            };
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAConcept',) !== -1) {
-            return {
-                color: '#00FF00'
-            };
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAOtherConcept') !== -1) {
-            return {
-                color: '#00FF00'
-            };
-        //METADATA
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataYearPublication') !== -1) {
-            return {
-                color: '#A9A9A9'
-            };
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataChronology') !== -1) {
-            return {
-                color: '#A9A9A9'
-            };
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataTopic') !== -1) {
-            return {
-                color: '#A9A9A9'
-            };
-        } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataTypology') !== -1) {
-            return {
-                color: '#A9A9A9'
-            };
-        } else {
-            return {
-                color: '#046380'
-            };
-        }
+    {
+      code: 'en',
+      label: 'Inglese'
     },
+    // {
+    //     code: 'de',
+    //     label: 'Tedesco'
+    // },
+    // {
+    //     code: 'ru',
+    //     label: 'Russo'
+    // },
+  ],
+  language: 'it',
+  viewOptions: {
+    onIriClick: ({
+      iri
+    }) => window.open(iri),
+    // groupBy: [{
+    //     linkType: 'http://www.researchspace.org/ontology/group',
+    //     linkDirection: 'in'
+    // }, ],
+  },
+  typeStyleResolver: types => {
+    //BOOK
+    if (types.indexOf('http://schema.org/LERMAbook') !== -1) {
+      return {
+        color: '#80040a'
+      };
+      //CONCEPTS
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMATopConcept') !== -1) {
+      return {
+        color: '#00FF00'
+      };
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAConcept', ) !== -1) {
+      return {
+        color: '#00FF00'
+      };
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAOtherConcept') !== -1) {
+      return {
+        color: '#00FF00'
+      };
+      //METADATA
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataYearPublication') !== -1) {
+      return {
+        color: '#A9A9A9'
+      };
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataChronology') !== -1) {
+      return {
+        color: '#A9A9A9'
+      };
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataTopic') !== -1) {
+      return {
+        color: '#A9A9A9'
+      };
+    } else if (types.indexOf('http://www.w3.org/2002/07/owl#LERMAMetadataTypology') !== -1) {
+      return {
+        color: '#A9A9A9'
+      };
+    } else {
+      return {
+        color: '#046380'
+      };
+    }
+  },
 };
 
 
@@ -157,8 +157,8 @@ const props = {
 // tutorial come integrare più componenti come comporre a partire da un componente
 
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.createElement('div');
-    container.id = 'root';
-    document.body.appendChild(container);
-    ReactDOM.render(React.createElement(Ontodia.Workspace, props), container)
+  const container = document.createElement('div');
+  container.id = 'root';
+  document.body.appendChild(container);
+  ReactDOM.render(React.createElement(Ontodia.Workspace, props), container)
 });
