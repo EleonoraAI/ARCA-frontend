3e141f6bff843a6113b0980f7ee774311cd9a7bf
Eleonora Bernasconi <eleonorameetup@gmail.com>
2019-10-31 13:24:28 +0100

    2019 10 31 - update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Ontodia = require('@eleonoraai/ontodia');
+var Ontodia = require('@eleonoraai/ontodia-arca');
 
 function onWorkspaceMounted(workspace) {
   if (!workspace) {
